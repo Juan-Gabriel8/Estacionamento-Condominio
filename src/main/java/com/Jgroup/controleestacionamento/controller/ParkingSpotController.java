b5168c4b5d34cafdc9405e6bb1363f07d5be77a4
Juan-Gabriel8 <juan.gabriel.m710@gmail.com>
2023-05-21 23:39:05 -0300

    Save ParkingSpot

--- src/main/java/com/Jgroup/controleestacionamento/controller/ParkingSpotController.java
+++ src/main/java/com/Jgroup/controleestacionamento/controller/ParkingSpotController.java
@@ -1,4 +1,32 @@
 package com.Jgroup.controleestacionamento.controller;
 
+import com.Jgroup.controleestacionamento.dto.ParkingSpotDto;
+import com.Jgroup.controleestacionamento.model.ParkingSpotModel;
+import com.Jgroup.controleestacionamento.service.ParkingSpotService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
 public class ParkingSpotController {
+
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
 }
--- src/main/java/com/Jgroup/controleestacionamento/service/ParkingSpotService.java
+++ src/main/java/com/Jgroup/controleestacionamento/service/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.Jgroup.controleestacionamento.service;
 
+import com.Jgroup.controleestacionamento.model.ParkingSpotModel;
 import com.Jgroup.controleestacionamento.repository.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +11,9 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel){
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
